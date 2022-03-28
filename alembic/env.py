f0b1011928d2b84adab53e61acc6c00a4cc4fdf2
ghouston16 <ghouston16@hotmail.com>
2022-03-28 01:36:29 +0100

    linting fix

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,8 @@
-from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config, pool
-
 from alembic import context
 from app.config import settings
-
-# Import Base for database.py to access SQL models
 from app.models import Base
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
