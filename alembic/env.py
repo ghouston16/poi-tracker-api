a603d508c01ee51b1ce49ebbe5e0b112ef000a6a
ghouston16 <ghouston16@hotmail.com>
2022-03-28 01:51:47 +0100

    commit

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,10 @@
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config, pool
+
 from alembic import context
 from app.config import settings
 from app.models import Base
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
