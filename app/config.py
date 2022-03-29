d9949677057d83358b9d2742713f8f30d8f6b6f6
ghouston16 <ghouston16@hotmail.com>
2022-03-29 23:22:56 +0100

    trial and error

--- app/config.py
+++ app/config.py
@@ -1,15 +1,16 @@
+import os
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_username: str
-    database_name: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    database_hostname: str = os.environ.getenv("DATABASE_HOSTNAME")
+    database_port: str = os.environ.getenv("DATABASE_PORT")
+    database_password: str = os.environ.getenv("DATABASE_PORT")
+    database_username: str = os.environ.getenv("DATABASE_USERNAME")
+    database_name: str = os.environ.getenv("DATABASE_NAME")
+    secret_key: str = os.environ.getenv("SECRET_KEY")
+    algorithm: str = os.environ.getenv("ALGORITHM")
+    access_token_expire_minutes: int = os.environ.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
     database_url: str
 
     class Config:
