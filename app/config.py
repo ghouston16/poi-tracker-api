c12682eead412d06e117cca8861d6556b0984727
ghouston16 <ghouston16@hotmail.com>
2022-03-31 03:57:02 +0100

    fix both pipelines

--- app/config.py
+++ app/config.py
@@ -1,18 +1,14 @@
-import os
-
 from pydantic import BaseSettings
 
-
 class Settings(BaseSettings):
-    database_hostname: str #= os.environ.get("DATABASE_HOSTNAME")
-    database_port: str #= os.environ.get("DATABASE_PORT")
-    database_password: str #= os.environ.get("DATABASE_PORT")
-    database_username: str #= os.environ.get("DATABASE_USERNAME")
-    database_name: str #= os.environ.get("DATABASE_NAME")
-    secret_key: str #= os.environ.get("SECRET_KEY")
-    algorithm: str #= os.environ.get("ALGORITHM")
-    access_token_expire_minutes: int #= os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
-    database_url: str# = os.environ.get("DATABASE_URL")
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_username: str
+    database_name: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
 
     class Config:
         env_file = ".env"
