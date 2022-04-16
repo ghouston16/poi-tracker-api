4ab0f444febc367999f33ebca7fcdc29cf4dc427
ghouston16 <ghouston16@hotmail.com>
2022-04-16 02:58:55 +0100

    fix linting issue

--- app/database.py
+++ app/database.py
@@ -1,20 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-from .config import settings # Import settings
+# Import settings
+from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base() # Declare a Base class for declarative class definitions
+# Declare a Base class for declarative class definitions
+Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal() # Create a session
+    # Create a session
+    db = SessionLocal()
     try:
-        yield db # Yield the session
+        # Yield the session
+        yield db
     finally:
-        db.close() # Close the session
+        # Close the session
+        db.close()
