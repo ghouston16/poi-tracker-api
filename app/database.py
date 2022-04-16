784d5d28996a85f4e11368fc9cf90ae2d3bb178d
ghouston16 <ghouston16@hotmail.com>
2022-04-16 02:51:14 +0100

    linting

--- app/database.py
+++ app/database.py
@@ -2,19 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .config import settings  # Import settings
+from .config import settings # Import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()  # Declare a Base class for declarative class definitions
+Base = declarative_base() # Declare a Base class for declarative class definitions
 
 
 def get_db():
-    db = SessionLocal()  # Create a session
+    db = SessionLocal() # Create a session
     try:
-        yield db  # Yield the session
+        yield db # Yield the session
     finally:
-        db.close()  # Close the session
+        db.close() # Close the session
